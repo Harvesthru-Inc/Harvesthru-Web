b0288f26b66f3f1cc26b00283a6d50b9202568f0
Bryan Yuan <bryanyuan1@Bryans-MacBook-Air.local>
2019-10-31 12:01:10 -0700

    debug post request in progress

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,18 @@
 import React from "react";
+import axios from "axios";
 class Home extends React.Component {
+  state = {};
+  componentDidMount() {
+    axios
+      .post("http://localhost:8000/api/read", {
+        name: "Ribeira Charming Duplex"
+      })
+      .then((res) => res.json())
+      .then((res) => this.setState({ house: res }));
+  }
+
   render() {
+    console.log(this.state.house);
     return <div>This is the home</div>;
   }
 }
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 const app = express();
 const path = require("path");
+const router = express.Router();
+const ObjectId = require("mongodb").ObjectId;
 
 // connect and get mongodb client object
 var database;
@@ -12,20 +15,37 @@ MongoClient.connect(URL, function(err, client) {
   if (err) {
     console.log("Error occurred while connecting to MongoDB Atlas...\n", err);
   }
-  database = client;
+  database = client.db("sample_airbnb");
 });
 
+// append /api for our http requests
+app.use("/api", router);
 // ... other app.use middleware
 app.use(express.static(path.join(__dirname, "client", "build")));
+// parse request body as json
+app.use(bodyParser.json());
 
 // routers for HTTP requests
-// app.get("/", ...)
+// example: retrieve data using name
+router.post("/read", (req, res) => {
+  // let cursor = database.collection("listingsAndReviews");
+  // cursor.find({ name: req.body.name }).toArray((error, documents) => {
+  //   if (error) throw error;
+  //   res.send(documents);
+  // });
+  console.log(req);
+  return res.json({ fuck: "fuck" });
+});
+
+router.post("/add", (req, res) => {
+  return res.json(undefined);
+});
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-let port = process.env.PORT || 8080;
+let port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log("listen on port 8000");
 });
