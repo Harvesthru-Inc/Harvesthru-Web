01ad5e34e3eaeb16dd79a25cc3233764a3f8db22
Bryan Yuan <bryanyuan1@bryans-air.home>
2019-11-02 17:15:32 -0700

    finished signup process

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,40 +1,10 @@
 import React from "react";
-import axios from "axios";
 class Home extends React.Component {
-  state = {
-    houses: []
-  };
-
-  // this function find object from the database using name.
-  // return is an array containing all matching ones
-  getHouseWithName = (name) => {
-    axios
-      .post("http://harvesthru-web.herokuapp.com/read", {
-        name: name
-      })
-      .then((res) => this.setState({ houses: res.data }));
-  };
-  componentDidMount() {
-    let name = "Ribeira Charming Duplex";
-    this.getHouseWithName(name);
-  }
-
   render() {
     if (this.state.houses === []) {
       return <div>Loading</div>;
     }
-    return (
-      <div>
-        {this.state.houses.map((house) => {
-          return (
-            <div>
-              <div>{house.name}</div>
-              <div>{house.summary}</div>
-            </div>
-          );
-        })}
-      </div>
-    );
+    return <div>this is the home page</div>;
   }
 }
 
--- server.js
+++ server.js
@@ -1,42 +1,25 @@
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+const mongoose = require("mongoose");
+const users = require("./routes/users");
 const express = require("express");
-const bodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
 const app = express();
 const path = require("path");
-const ObjectId = require("mongodb").ObjectId;
 
-// connect and get mongodb client object
-var database;
-const URL =
-  "mongodb+srv://harvesthruadmin:Entrepreneurs007%24@harvesthru-back-xwbyo.mongodb.net/test?retryWrites=true";
-MongoClient.connect(URL, function(err, client) {
-  if (err) {
-    console.log("Error occurred while connecting to MongoDB Atlas...\n", err);
-  }
-  database = client.db("sample_airbnb");
-});
+// connect using mongoose
+const URL = `mongodb+srv://yiyuan:20000805@harvesthru-back-xwbyo.mongodb.net/test?retryWrites=true`;
+
+mongoose
+  .connect(URL, { dbName: "authentication" })
+  .then(() => console.log("Now connected to MongoDB!"))
+  .catch((err) => console.error("Something went wrong", err));
 
 // ... other app.use middleware
 app.use(express.static(path.join(__dirname, "client", "build")));
 // parse request body as json
-app.use(bodyParser.json());
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-
-// routers for HTTP requests
-// example: retrieve data using name
-app.post("/read", (req, res) => {
-  let cursor = database.collection("listingsAndReviews");
-  cursor.find({ name: req.body.name }).toArray((error, documents) => {
-    if (error) throw error;
-    res.send(documents);
-  });
-});
+app.use(express.json());
+// using registration route
+app.use("/api/users", users);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
