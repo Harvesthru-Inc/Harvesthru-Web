8c43ceac4fc9ed98f3a46b22efafe25a52815311
Jaden Padua <paduajaden@gmail.com>
2019-11-11 22:36:00 -0800

    change config.get to process env variables in auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const Joi = require("joi");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
@@ -26,7 +27,7 @@ router.post("/", async (req, res) => {
   if (!validPassword) {
     return res.status(400).send("Incorrect email or password.");
   }
-  const token = jwt.sign({ _id: user._id }, config.get("PrivateKey")); // PrivateKey in config/default.json
+  const token = jwt.sign({ _id: user._id },process.env.privateKey); // PrivateKey in config/default.json
   res.header("x-auth-token", token).send({
     _id: user._id,
     name: user.name,
