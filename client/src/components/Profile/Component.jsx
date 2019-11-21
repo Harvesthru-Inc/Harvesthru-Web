955410caa2b1263661294c454ac887f006179cb2
bryanyuan1 <yiyuan@ucsd.edu>
2019-11-21 12:08:00 -0800

    added sidebar item

--- client/src/components/Profile/Component.jsx
+++ client/src/components/Profile/Component.jsx
@@ -3,17 +3,41 @@ import styled from 'styled-components';
 
 const NAV_HEIGHT = 100;
 const DASHBOARD_WIDTH = 247;
+const TEXT_COLOR = '#4A4A4A';
+const BORDER_COLOR = '#BFB8B8';
 
 class Profile extends React.Component {
   render() {
     return (
       <FullPageLayer>
-        <DashBoardContainer>DashBoard</DashBoardContainer>
+        <DashboardContainer>
+          <DashboardItem>
+            <DashboardTitle>My Dashboard</DashboardTitle>
+          </DashboardItem>
+          <DashboardItem></DashboardItem>
+        </DashboardContainer>
         <ProfileMainContainer></ProfileMainContainer>
       </FullPageLayer>
     );
   }
 }
+const DashboardTitle = styled.div`
+  font-family: Quicksand;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 26px;
+  line-height: 32px;
+  /* identical to box height */
+
+  color: ${TEXT_COLOR};
+`;
+
+const DashboardItem = styled.div`
+  padding: 20px 35px 20px 32px;
+  width: ${DASHBOARD_WIDTH}px;
+  border-bottom: 1px solid ${BORDER_COLOR};
+  box-sizing: border-box;
+`;
 
 const ProfileMainContainer = styled.div`
   position: absolute;
@@ -29,7 +53,7 @@ const FullPageLayer = styled.div`
   padding-top: ${NAV_HEIGHT}px;
 `;
 
-const DashBoardContainer = styled.div`
+const DashboardContainer = styled.div`
   position: absolute;
   width: ${DASHBOARD_WIDTH}px;
   left: 0px;
@@ -37,7 +61,7 @@ const DashBoardContainer = styled.div`
   bottom: 0px;
 
   background: #ffffff;
-  border: 1px solid #bfb8b8;
+  border: 1px solid ${BORDER_COLOR};
   box-sizing: border-box;
 
   display: flex;
