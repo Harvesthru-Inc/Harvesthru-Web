a542041cf8d847e7997c016462babbce1828a8ea
bryanyuan1 <yiyuan@ucsd.edu>
2019-11-21 12:37:08 -0800

    added arrow image to nav link

--- client/src/components/Profile/Component.jsx
+++ client/src/components/Profile/Component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import right_arrow from '../../assets/images/right_arrow.png';
 
 const NAV_HEIGHT = 100;
 const DASHBOARD_WIDTH = 247;
@@ -11,16 +12,25 @@ class Profile extends React.Component {
     return (
       <FullPageLayer>
         <DashboardContainer>
-          <DashboardItem>
+          <DashboardTitleBox>
             <DashboardTitle>My Dashboard</DashboardTitle>
+          </DashboardTitleBox>
+          <DashboardItem>
+            <DashboardText>My Profile</DashboardText>
+            <RightArrow src={right_arrow} />
           </DashboardItem>
-          <DashboardItem></DashboardItem>
         </DashboardContainer>
         <ProfileMainContainer></ProfileMainContainer>
       </FullPageLayer>
     );
   }
 }
+
+const RightArrow = styled.img`
+  width: 27px;
+  height: 27px;
+`;
+
 const DashboardTitle = styled.div`
   font-family: Quicksand;
   font-style: normal;
@@ -32,11 +42,37 @@ const DashboardTitle = styled.div`
   color: ${TEXT_COLOR};
 `;
 
+const DashboardText = styled.div`
+  font-family: Quicksand;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+  cursor: pointer;
+  color: #000000;
+`;
+
 const DashboardItem = styled.div`
+  padding: 20px 10px 20px 32px;
+  width: ${DASHBOARD_WIDTH}px;
+  border-bottom: 1px solid ${BORDER_COLOR};
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const DashboardTitleBox = styled.div`
   padding: 20px 35px 20px 32px;
   width: ${DASHBOARD_WIDTH}px;
   border-bottom: 1px solid ${BORDER_COLOR};
   box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const ProfileMainContainer = styled.div`
