9a88bc648da3f4eee91a6670feaf34f0e7693a53
bryanyuan1 <yiyuan@ucsd.edu>
2019-11-21 21:13:02 -0800

    changed magic number to variables

--- client/src/components/Profile/Component.jsx
+++ client/src/components/Profile/Component.jsx
@@ -1,45 +1,39 @@
 import React from 'react';
-import right_arrow from '../../assets/images/right_arrow.png';
 import avatar_placeholder from '../../assets/images/avatar_placeholder.png';
 import email_icon from '../../assets/images/email_icon.png';
 import home_icon from '../../assets/images/home_icon.png';
 import star_icon from '../../assets/images/star_icon.png';
 import verified_icon from '../../assets/images/verified_icon.png';
 import edit_icon from '../../assets/images/edit_icon.png';
-import styled from 'styled-components';
 import './scroll.css';
 
 import {
-  DummyReview,
-  SectionContainer,
-  SectionTitle,
-  SectionContent,
   InfoListContainer,
   InfoListItemIcon,
   InfoListItemContainer,
   InfoListItemText,
-  StatsContainer,
-  SingleStatContainer,
-  StatLabel,
-  StatNumber,
   Avatar,
   AvatarText,
   SectionsContainer,
   MainContainer,
   PersonalInfoContainer,
-  Divider,
-  ViewAndEditProfile,
-  GreenText,
-  WelcomeMessage,
-  TopBar,
-  RightArrow,
-  DashboardTitle,
-  DashboardText,
-  DashboardItem,
-  DashboardTitleBox,
   ProfileMainContainer,
   FullPageLayer,
-  DashboardContainer
+  GridItemContainer,
+  GridContainer,
+  SeeAll,
+  GridNav,
+  GridNavItem,
+  EditIcon,
+  EditButton,
+  EditButtonRow,
+  FollowsContainer,
+  Followers,
+  Following,
+  FollowsLabel,
+  FollowsNumber,
+  Description,
+  NameText
 } from './styled.jsx';
 
 class Profile extends React.Component {
@@ -130,141 +124,5 @@ class Profile extends React.Component {
     );
   }
 }
-
-const GridScroller = styled.div`
-  width: 100%;
-  overflow-y: scroll;
-`;
-
-const GridItemContainer = styled.div`
-  width: 360px;
-  height: 200px;
-  background: #c4c4c4;
-  border-radius: 8px;
-`;
-
-const GridContainer = styled.div`
-  margin-top: 24px;
-  display: grid;
-  grid-auto-flow: column;
-  grid-row-gap: 50px;
-  overflow-y: scroll;
-  height: 800px;
-`;
-
-const SeeAll = styled.div`
-  font-family: Quicksand;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 20px;
-  color: #9acd00;
-  width: 100%;
-  text-align: right;
-`;
-
-const GridNav = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 500px;
-`;
-
-const GridNavItem = styled.div`
-  display: inline-block;
-  font-family: Quicksand;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 22px;
-  line-height: 27px;
-  color: #4a4a4a;
-  cursor: pointer;
-`;
-
-const EditIcon = styled.img`
-  width: 14px;
-  height: 14px;
-  margin-right: 5px;
-`;
-
-const EditButton = styled.div`
-  border: 1px solid #9acd00;
-  box-sizing: border-box;
-  border-radius: 24px;
-  width: 127px;
-  height: 32px;
-  font-family: Quicksand;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 20px;
-  padding-top: 5px;
-  padding-left: 12px;
-  cursor: pointer;
-  color: #9acd00;
-`;
-
-const EditButtonRow = styled.div`
-  width: 285px;
-  display: flex;
-  justify-content: flex-end;
-`;
-
-const FollowsContainer = styled.div`
-  width: 200px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Followers = styled.div`
-  display: inline-block;
-`;
-
-const Following = styled.div`
-  display: inline-block;
-`;
-
-const FollowsLabel = styled.div`
-  font-family: Montserrat;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 14px;
-  line-height: 17px;
-  display: inline-block;
-  color: #4a4a4a;
-`;
-
-const FollowsNumber = styled.div`
-  font-family: Montserrat;
-  font-style: normal;
-  font-weight: 700;
-  font-size: 14px;
-  line-height: 17px;
-  display: inline-block;
-  color: #4a4a4a;
-`;
-
-const Description = styled.div`
-  font-family: Montserrat;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 14px;
-  line-height: 17px;
-  width: 228px;
-  color: #4a4a4a;
-  margin: 18px 0px 16px 0px;
-`;
-
-const NameText = styled.div`
-  font-family: Quicksand;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 26px;
-  line-height: 32px;
-  /* identical to box height */
-
-  color: #4a4a4a;
-`;
 
 export default Profile;
--- client/src/components/Profile/styled.jsx
+++ client/src/components/Profile/styled.jsx
@@ -262,35 +262,162 @@ const DashboardContainer = styled.div`
   justify-content: flex-start;
 `;
 
+const GridItemContainer = styled.div`
+  width: 360px;
+  height: 200px;
+  background: #c4c4c4;
+  border-radius: 8px;
+`;
+
+const GridContainer = styled.div`
+  margin-top: 24px;
+  display: grid;
+  grid-auto-flow: column;
+  grid-row-gap: 50px;
+  overflow-y: scroll;
+  height: 800px;
+`;
+
+const SeeAll = styled.div`
+  font-family: Quicksand;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 20px;
+  color: ${THEME_GREEN_COLOR};
+  width: 100%;
+  text-align: right;
+`;
+
+const GridNav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 500px;
+`;
+
+const GridNavItem = styled.div`
+  display: inline-block;
+  font-family: Quicksand;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 27px;
+  color: ${TEXT_COLOR};
+  cursor: pointer;
+`;
+
+const EditIcon = styled.img`
+  width: 14px;
+  height: 14px;
+  margin-right: 5px;
+`;
+
+const EditButton = styled.div`
+  border: 1px solid ${THEME_GREEN_COLOR};
+  box-sizing: border-box;
+  border-radius: 24px;
+  width: 127px;
+  height: 32px;
+  font-family: Quicksand;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 20px;
+  padding-top: 5px;
+  padding-left: 12px;
+  cursor: pointer;
+  color: ${THEME_GREEN_COLOR};
+`;
+
+const EditButtonRow = styled.div`
+  width: 285px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const FollowsContainer = styled.div`
+  width: 200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Followers = styled.div`
+  display: inline-block;
+`;
+
+const Following = styled.div`
+  display: inline-block;
+`;
+
+const FollowsLabel = styled.div`
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 17px;
+  display: inline-block;
+  color: ${TEXT_COLOR};
+`;
+
+const FollowsNumber = styled.div`
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 17px;
+  display: inline-block;
+  color: ${TEXT_COLOR};
+`;
+
+const Description = styled.div`
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 17px;
+  width: 228px;
+  color: ${TEXT_COLOR};
+  margin: 18px 0px 16px 0px;
+`;
+
+const NameText = styled.div`
+  font-family: Quicksand;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 26px;
+  line-height: 32px;
+  /* identical to box height */
+
+  color: ${TEXT_COLOR};
+`;
+
 export {
-  DummyReview,
-  SectionContainer,
-  SectionTitle,
-  SectionContent,
   InfoListContainer,
   InfoListItemIcon,
   InfoListItemContainer,
   InfoListItemText,
-  StatsContainer,
-  SingleStatContainer,
-  StatLabel,
-  StatNumber,
   Avatar,
   AvatarText,
   SectionsContainer,
   MainContainer,
   PersonalInfoContainer,
-  Divider,
-  ViewAndEditProfile,
-  GreenText,
-  WelcomeMessage,
-  TopBar,
-  RightArrow,
-  DashboardTitle,
-  DashboardText,
-  DashboardItem,
-  DashboardTitleBox,
   ProfileMainContainer,
   FullPageLayer,
-  DashboardContainer
+  GridItemContainer,
+  GridContainer,
+  SeeAll,
+  GridNav,
+  GridNavItem,
+  EditIcon,
+  EditButton,
+  EditButtonRow,
+  FollowsContainer,
+  Followers,
+  Following,
+  FollowsLabel,
+  FollowsNumber,
+  Description,
+  NameText
 };
