7e0d67764dfe5360355d9acb991004b5f393bbfd
bryanyuan1 <yiyuan@ucsd.edu>
2019-11-21 21:14:23 -0800

    removed unused components

--- client/src/components/Profile/styled.jsx
+++ client/src/components/Profile/styled.jsx
@@ -7,37 +7,6 @@ const BORDER_COLOR = '#BFB8B8';
 const THEME_GREEN_COLOR = '#9acd00';
 const SECTION_HEIGHT = 225;
 
-const DummyReview = styled.div`
-  margin-bottom: 15px;
-`;
-
-const SectionContainer = styled.div`
-  display: block;
-`;
-
-const SectionTitle = styled.div`
-  font-family: Quicksand;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 22px;
-  line-height: 27px;
-  /* identical to box height */
-
-  color: ${TEXT_COLOR};
-`;
-
-const SectionContent = styled.div`
-  font-family: Montserrat;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 16px;
-  line-height: 20px;
-  padding: 16px 24px 16px 24px;
-  color: ${TEXT_COLOR};
-  height: ${SECTION_HEIGHT}px;
-  overflow-y: scroll;
-`;
-
 const InfoListContainer = styled.div`
   width: 200px;
 `;
@@ -66,42 +35,6 @@ const InfoListItemText = styled.div`
   color: {TEXT_COLOR};
   margin-left: 19px;
   display: inline-block;
-`;
-
-const StatsContainer = styled.div`
-  width: 290px;
-  height: 50px;
-  margin-top: 30px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const SingleStatContainer = styled.div`
-  text-align: center;
-`;
-
-const StatLabel = styled.div`
-  font-family: Quicksand;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 16px;
-  line-height: 20px;
-  /* identical to box height */
-
-  color: ${TEXT_COLOR};
-`;
-
-const StatNumber = styled.div`
-  font-family: Quicksand;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 22px;
-  line-height: 27px;
-  /* identical to box height */
-
-  color: ${TEXT_COLOR};
 `;
 
 const Avatar = styled.img`
@@ -144,93 +77,6 @@ const PersonalInfoContainer = styled.div`
   padding: 0px 40px 0px 40px;
 `;
 
-const Divider = styled.div`
-  width: 25px;
-  height: 0px;
-  border: 1px solid #999988;
-  transform: translateY(8px) rotate(90deg);
-`;
-
-const ViewAndEditProfile = styled.div`
-  height: 20px;
-  display: flex;
-`;
-
-const GreenText = styled.div`
-  font-family: Quicksand;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 20px;
-  display: inline-block;
-  color: ${THEME_GREEN_COLOR};
-`;
-
-const WelcomeMessage = styled.div`
-  font-family: Quicksand;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 26px;
-  line-height: 32px;
-`;
-
-const TopBar = styled.div`
-  width: 100%;
-  height: 35px;
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-start;
-`;
-
-const RightArrow = styled.img`
-  width: 27px;
-  height: 27px;
-`;
-
-const DashboardTitle = styled.div`
-  font-family: Quicksand;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 26px;
-  line-height: 32px;
-  /* identical to box height */
-
-  color: ${TEXT_COLOR};
-`;
-
-const DashboardText = styled.div`
-  font-family: Quicksand;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 18px;
-  line-height: 22px;
-  cursor: pointer;
-  color: #000000;
-`;
-
-const DashboardItem = styled.div`
-  padding: 20px 10px 20px 32px;
-  width: ${DASHBOARD_WIDTH}px;
-  border-bottom: 1px solid ${BORDER_COLOR};
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  cursor: pointer;
-`;
-
-const DashboardTitleBox = styled.div`
-  padding: 20px 35px 20px 32px;
-  width: ${DASHBOARD_WIDTH}px;
-  border-bottom: 1px solid ${BORDER_COLOR};
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
-
 const ProfileMainContainer = styled.div`
   position: absolute;
   left: ${DASHBOARD_WIDTH}px;
@@ -244,22 +90,6 @@ const FullPageLayer = styled.div`
   width: 100%;
   height: 100vh;
   padding-top: ${NAV_HEIGHT}px;
-`;
-
-const DashboardContainer = styled.div`
-  position: fixed;
-  width: ${DASHBOARD_WIDTH}px;
-  left: 0px;
-  top: ${NAV_HEIGHT}px;
-  bottom: 0px;
-
-  background: #ffffff;
-  border: 1px solid ${BORDER_COLOR};
-  box-sizing: border-box;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
 `;
 
 const GridItemContainer = styled.div`
