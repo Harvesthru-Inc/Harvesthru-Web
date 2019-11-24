f8191eaeedd1e9942317c2da4cd0303ccfed9af6
hwanggit <luw055@ucsd.edu>
2019-11-24 11:37:16 -0800

    changed redirect url

--- client/src/static/constants/appPath.js
+++ client/src/static/constants/appPath.js
@@ -1,6 +1,6 @@
 // To do (Dennis): change it to the real path.
 export const PATH_FORGOT_PASSWORD = '/';
 export const REACT_APP_FB_APP_ID="793914657709001";
-export const REACT_APP_FB_REDIRECT_URL="https://localhost:8000";
+export const REACT_APP_FB_REDIRECT_URL="https://harvesthru-web.herokuapp.com/";
 export const REACT_APP_FB_LOGIN_DIALOG="https://www.facebook.com/v5.0/dialog/oauth?";
 export const REACT_APP_FB_STATE_KEY="fb-login-secret"
